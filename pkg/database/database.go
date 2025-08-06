ae583e2e2f1affd091b94eb9d312281bf4afd3e8
Hadi T <hadi@syniol.com>
2025-08-06 17:37:50 +0100

    changes to database

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -3,7 +3,9 @@ package database
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
+	"sync"
 
 	"database/sql"
 )
@@ -13,36 +15,35 @@ type Database struct {
 	*sql.DB
 }
 
+var once sync.Once
 var instance *Database
 
 func NewDatabase(ctx context.Context) (*Database, error) {
-	if instance != nil {
-		return instance, nil
-	}
+	once.Do(func() {
+		// https://www.postgresql.org/docs/current/libpq-ssl.html
+		connStr := fmt.Sprintf(
+			"postgresql://%s:%s@%s/%s?sslmode=require",
+			os.Getenv("POSTGRES_USER"),
+			os.Getenv("POSTGRES_PASSWORD"),
+			os.Getenv("POSTGRES_DB"),
+			func() string {
+				if len(os.Getenv("DEBUG")) > 0 {
+					return "127.0.0.1"
+				}
 
-	// https://www.postgresql.org/docs/current/libpq-ssl.html
-	connStr := fmt.Sprintf(
-		"postgresql://%s:%s@%s/%s?sslmode=require",
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("POSTGRES_DB"),
-		func() string {
-			if len(os.Getenv("DEBUG")) > 0 {
-				return "127.0.0.1"
-			}
+				return "database"
+			}(),
+		)
+		cnn, err := sql.Open("postgres", connStr)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-			return "database"
-		}(),
-	)
-	cnn, err := sql.Open("postgres", connStr)
-	if err != nil {
-		return nil, err
-	}
-
-	instance = &Database{
-		Ctx: ctx,
-		DB:  cnn,
-	}
+		instance = &Database{
+			Ctx: ctx,
+			DB:  cnn,
+		}
+	})
 
 	return instance, nil
 }
